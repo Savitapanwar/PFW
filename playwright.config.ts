88e91c5a2f68648667892934e6909a426ceb3e07
Savita Panwar <savita.panwar2911@gmail.com>
2026-04-18 01:22:59 +0530

    updated CI config

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,16 +16,16 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  /*forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /*retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /*workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Global timeout settings */
   timeout: 60 * 1000, // 60 seconds per test
-  navigationTimeout: 30 * 1000, // 30 seconds for navigation
+  /*navigationTimeout: 30 * 1000, // 30 seconds for navigation
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
